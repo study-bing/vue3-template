e1bec97157a20d63f645a11e72f8322a71e9dc11
林斌 <linbin@ruhrtec.cn>
2022-02-26 10:09:02 +0800

    fix: 文件名改为大写

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     env: {
-        node: true
+        node: true,
     },
     extends: ['plugin:vue/essential', 'eslint:recommended'],
     rules: {
@@ -14,8 +14,8 @@ module.exports = {
         camelcase: [
             'error',
             {
-                properties: 'never'
-            }
+                properties: 'never',
+            },
         ], //强制驼峰法命名
         eqeqeq: 'error', //  #使用带引号替代双引号
         'no-extend-native': 2, //禁止扩展native对象
@@ -24,8 +24,8 @@ module.exports = {
         'no-multiple-empty-lines': [
             1,
             {
-                max: 2
-            }
+                max: 2,
+            },
         ], //空行最多不能超过2行
         'no-native-reassign': 2, //不能重写native对象
         'no-trailing-spaces': 1, //一行结束后面不要有空格
@@ -34,28 +34,37 @@ module.exports = {
         'vue/no-parsing-error': [
             2,
             {
-                'x-invalid-end-tag': false
-            }
+                'x-invalid-end-tag': false,
+            },
         ],
         'no-dupe-keys': 2, //在创建对象字面量时不允许键重复 {a:1,a:1}
         'no-dupe-args': 2, //函数参数不能重复
-        "prefer-template":"error",//以编程方式构建字符串时，使用模板字符串而不是连接符 例:How are you, ${name}?
-        "prefer-arrow-callback":"error",//当你必须要使用匿名函数（如在传递内联回调时），请使用箭头函数
-        "no-dupe-class-members":"error",//避免重复的类成员
-        "no-duplicate-imports":"error",//同一个路径只使用一次import
-        "vue/html-closing-bracket-newline": ["error", {
-            "singleline": "never",//.单行元素的配置禁止在右括号前换行
-            "multiline": "always"//多行元素的配置要求在右方括号前换行。
-        }],
-        "no-var":"error",//不得申明var用let
-        'vue/html-closing-bracket-spacing': ["error", {
-            "startTag": "never",
-            "endTag": "never",
-            "selfClosingTag": "never"
-        }],
-        "vue/no-multiple-template-root": "off"
+        'prefer-template': 'error', //以编程方式构建字符串时，使用模板字符串而不是连接符 例:How are you, ${name}?
+        'prefer-arrow-callback': 'error', //当你必须要使用匿名函数（如在传递内联回调时），请使用箭头函数
+        'no-dupe-class-members': 'error', //避免重复的类成员
+        'no-duplicate-imports': 'error', //同一个路径只使用一次import
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never', //.单行元素的配置禁止在右括号前换行
+                multiline: 'always', //多行元素的配置要求在右方括号前换行。
+            },
+        ],
+        'no-var': 'error', //不得申明var用let
+        'vue/html-closing-bracket-spacing': [
+            'error',
+            {
+                startTag: 'never',
+            },
+        ],
+        'vue/no-multiple-template-root': 'off',
     },
     parserOptions: {
-        parser: 'babel-eslint'
-    }
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
 }
